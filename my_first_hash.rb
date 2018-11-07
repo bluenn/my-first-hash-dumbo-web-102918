739c4e65ecd44f10b301bb41947b31d15892ecaa
a lan <bluenowte97@gmail.com>
2018-11-07 22:21:11 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,23 +25,20 @@ def retrieval
     "oil paintings" => 3
                  }
 
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 end
 
 def adding_shipping_manifest =
   the_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+  the_manifest["muskets"] = 2
+  the_manifest["gun poweder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
   # return the shipping_manifest hash below
-
+  the_manifest
 end
